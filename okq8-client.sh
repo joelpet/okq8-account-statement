29c6f087633d1c8e503bfd2769ae6f88052e79e5
Joel Pettersson <petterssonjoel@gmail.com>
2014-04-05 01:06:00 +0200

    Add PDF to TXT conversion

--- okq8-client.sh
+++ okq8-client.sh
@@ -3,4 +3,13 @@
 read -p "User: " user
 read -s -p "Password: " password
 
+echo -e "\nPlease wait..."
+
 casperjs ./scraper/okq8-scraper.js --user="$user" --password="$password"
+
+if [ $? -ne 0 ]; then
+    echo "Scraper failed"
+    exit
+fi
+
+pdftotext -layout statement.pdf
